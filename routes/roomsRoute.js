a255f7b31e3ee789ca3962cca0856556f9da9482
Mithun Prakash <43476072+mithunprakash15@users.noreply.github.com>
2023-03-16 17:28:48 +0530

    Update roomsRoute.js

--- routes/roomsRoute.js
+++ routes/roomsRoute.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 router.use(cors());
 const bp = require("body-parser");
 router.use(express.json());
-router.use(express.urlencoded());
+router.use(express.urlencoded({ extended: true } ));
 
 router.get("/getAllRooms", async (req, res) => {
   try {
